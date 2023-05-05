5c5c260841ebb4e4d731751a209570a94457899b
Roger <rsobrino@xtec.cat>
2023-05-05 16:27:55 +0200

    Modificacions degut error <no module named 'django.templates'

--- DJ_Rest_Framework/DJ_Rest_Framework/settings.py
+++ DJ_Rest_Framework/DJ_Rest_Framework/settings.py
@@ -55,15 +55,15 @@ ROOT_URLCONF = 'DJ_Rest_Framework.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.templates.backends.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates'
+            BASE_DIR, 'templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.templates.context_processors.debug',
-                'django.templates.context_processors.request',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
--- DJ_Rest_Framework/api_app/views.py
+++ DJ_Rest_Framework/api_app/views.py
@@ -1,8 +1,6 @@
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.renderers import TemplateHTMLRenderer
-from rest_framework.views import APIView
+
 
 
 """
